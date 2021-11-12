300c6d9aab66939b9a040a0e17fb712553b5a4f6
Malte Londschien <61679398+mlondschien@users.noreply.github.com>
2021-11-12 15:41:08 +0100

    Add score. (#4)

---
+++ changeforest_simulations/score.py
@@ -0,0 +1,27 @@
+import numpy as np
+from sklearn.metrics import adjusted_rand_score as sklearn_adjusted_rand_score
+
+
+def adjusted_rand_score(true_changepoints, estimated_changepoints):
+    """Compute the adjusted rand index between two sets of changepoints.
+
+    Uses sklearn.metrics.adjusted_rand_score under the hood. See their documentation:
+    https://scikit-learn.org/stable/modules/generated/sklearn.metrics.adjusted_rand_score.html
+
+    """
+    true_changepoints = np.array(true_changepoints, dtype=np.int_)
+    estimated_changepoints = np.array(estimated_changepoints, dtype=np.int_)
+
+    y_true = np.zeros(true_changepoints[-1])
+    for i, (start, stop) in enumerate(
+        zip(true_changepoints[:-1], true_changepoints[1:])
+    ):
+        y_true[start:stop] = i
+
+    y_estimated = np.zeros(estimated_changepoints[-1])
+    for i, (start, stop) in enumerate(
+        zip(estimated_changepoints[:-1], estimated_changepoints[1:])
+    ):
+        y_estimated[start:stop] = i
+
+    return sklearn_adjusted_rand_score(y_true, y_estimated)
