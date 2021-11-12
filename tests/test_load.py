0256cebb049f253ff7803a95965529d71e74f3eb
Malte Londschien <malte@londschien.de>
2021-11-12 16:59:18 +0100

    More data.

--- tests/test_load.py
+++ tests/test_load.py
@@ -1,15 +1,21 @@
 import pytest
 
-from changeforest_simulations import load_iris, load_letters
+from changeforest_simulations import load
 
 
 @pytest.mark.parametrize(
-    "load, expected_shape", [(load_iris, (150, 5)), (load_letters, (20000, 17))]
+    "dataset, expected_shape",
+    [
+        ("iris", (150, 5)),
+        ("letters", (20000, 17)),
+        ("red_wine", (1599, 12)),
+        ("white_wine", (4898, 12)),
+    ],
 )
-def test_load(load, expected_shape):
-    data = load()
+def test_load(dataset, expected_shape):
+    data = load(dataset)
     assert data.shape == expected_shape
 
     # Load twice as we might have downloaded directly from openml the first time.
-    data = load()
+    data = load(dataset)
     assert data.shape == expected_shape
