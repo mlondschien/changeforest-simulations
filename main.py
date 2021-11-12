3a0f67614035eedad47ffdb3f30b9a67b14e908c
Malte Londschien <malte@londschien.de>
2021-11-12 17:14:30 +0100

    Update main script.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import logging
 from time import perf_counter
 
+import pandas as pd
 from hdcd import Control, hdcd
 
 from changeforest_simulations import adjusted_rand_score, load, simulate
@@ -9,15 +10,30 @@ logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
 seeds = list(range(5))
-datasets = ["iris", "letters"]
+datasets = [
+    ("iris", "class"),
+    ("letters", "class"),
+    ("red_wine", "quality"),
+    ("white_wine", "quality"),
+]
 
-# results = pd.DataFrame(columns=["dataset", "seed", "segmentation", "method", "true_changepoints", "estimated_changepoints", "score", "result"])
+result_columns = [
+    "dataset",
+    "seed",
+    "segmentation",
+    "method",
+    "true_changepoints",
+    "estimated_changepoints",
+    "score",
+    "result",
+]
+results = pd.DataFrame(columns=result_columns)
 
-for dataset in datasets:
+for dataset, class_label in datasets:
     data = load(dataset)
 
     for seed in seeds:
-        change_points, time_series = simulate(data, seed=seed)
+        change_points, time_series = simulate(data, seed=seed, class_label=class_label)
         logger.info(f"True changepoints: {list(change_points)}.")
 
         for segmentation in "bs", "sbs", "wbs":
@@ -32,8 +48,34 @@ for dataset in datasets:
                 )
                 toc = perf_counter()
 
+                estimated_changepoints = result.split_points()
+                score = adjusted_rand_score(
+                    change_points, [0] + result.split_points() + [len(time_series)]
+                )
+                results = results.append(
+                    pd.DataFrame(
+                        [
+                            [
+                                dataset,
+                                seed,
+                                segmentation,
+                                method,
+                                change_points,
+                                estimated_changepoints,
+                                score,
+                                result,
+                            ]
+                        ],
+                        columns=result_columns,
+                    )
+                )
+
                 logger.info(
                     f"Detection for dataset {dataset} seed {seed} method {method} and segmentation {segmentation} "
-                    f"found change points {result.split_points()} in {toc - tic:.2f}s. "
-                    f"Score={adjusted_rand_score(change_points, [0] + result.split_points() + [len(time_series)])}."
+                    f"found change points {estimated_changepoints} in {toc - tic:.2f}s. "
+                    f"Score={score}."
                 )
+    print(results.drop(columns="result"))
+
+breakpoint()
+print(results.drop(columns="result"))
