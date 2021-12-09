1db782ccd033cd35647371ef9f6443850e1b591b
Malte Londschien <malte@londschien.de>
2021-12-09 09:22:23 +0100

    Simplify main.py script.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import logging
+from datetime import datetime
+from pathlib import Path
 from time import perf_counter
-
-import pandas as pd
 
 from changeforest_simulations import adjusted_rand_score, load, simulate
 from changeforest_simulations.methods import estimate_changepoints
@@ -9,40 +9,32 @@ from changeforest_simulations.methods import estimate_changepoints
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
-seeds = list(range(1))
+now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+file_path = Path(__file__).parent.absolute() / "output" / f"{now}.csv"
+file_path.write_text("dataset,seed,method,score,time\n")
+
+seeds = list(range(250))
 
 datasets = [
     "iris",
-    "letters",
+    # "letters",
     "red_wine",
     "white_wine",
 ]
-
-result_columns = [
-    "dataset",
-    "seed",
-    "method",
-    "score",
-    "time",
-]
-results = pd.DataFrame(columns=result_columns)
 
 for dataset in datasets:
     data = load(dataset)
 
     for seed in seeds:
         change_points, time_series = simulate(data, seed=seed)
-        logger.info(f"True changepoints: {list(change_points)}.")
 
         for method in [
             "changeforest_bs",
-            "changeforest_sbs",
             "changekNN_bs",
-            "changekNN_sbs",
             "change_in_mean_bs",
-            "change_in_mean_sbs",
             "ecp",
             "multirank",
+            "kernseg",
         ]:
             tic = perf_counter()
             estimate = estimate_changepoints(
@@ -51,18 +43,5 @@ for dataset in datasets:
             toc = perf_counter()
 
             score = adjusted_rand_score(change_points, estimate)
-
-            results = results.append(
-                pd.DataFrame(
-                    [[dataset, seed, method, score, toc - tic]], columns=result_columns
-                )
-            )
-
-            logger.info(
-                f"Detection for dataset {dataset} seed {seed} method {method} "
-                f"found change points {estimate} in {toc - tic:.2f}s. "
-                f"Score={score}."
-            )
-
-
-print(results.groupby(["dataset", "method"])["score", "time"].mean())
+            with open(file_path, "a") as f:
+                f.write(f"{dataset},{seed},{method},{score},{toc-tic}\n")
