18459ef548d9f59be1743cfe457824747d59261a
Malte Londschien <malte@londschien.de>
2021-12-10 10:51:07 +0100

    An additional test.

--- tests/test_load.py
+++ tests/test_load.py
@@ -10,11 +10,14 @@ from changeforest_simulations import load
         ("letters", (20000, 17)),
         ("red_wine", (1599, 12)),
         ("white_wine", (4898, 12)),
+        ("wine", (1599 + 4898, 13)),
     ],
 )
 def test_load(dataset, expected_shape):
     data = load(dataset)
     assert data.shape == expected_shape
+
+    assert data.drop(columns="class").to_numpy().dtype == "float"  # no object columns
 
     # Load twice as we might have downloaded directly from openml the first time.
     data = load(dataset)
