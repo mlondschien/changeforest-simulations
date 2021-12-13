682d2bf9643f8e9b3c69df4b4354c7fe09182613
Malte Londschien <61679398+mlondschien@users.noreply.github.com>
2021-12-13 21:08:33 +0100

    Use click in display_results.py. (#17)

--- display_results.py
+++ display_results.py
@@ -1,21 +1,32 @@
 from pathlib import Path
 
+import click
 import numpy as np
 import pandas as pd
 
 output_path = Path(__file__).parent.absolute() / "output"
 files = output_path.glob("2021-*.csv")
-last_file = sorted(files)[-1]
 
-df = pd.read_csv(last_file)
 
-print("mean score\n")
-print(df.pivot_table(index="method", columns="dataset", values="score"))
-print("\n\nmedian score\n")
-print(
-    df.pivot_table(index="method", columns="dataset", values="score", aggfunc=np.median)
-)
-print("\n\nmean n_cpts\n")
-print(df.pivot_table(index="method", columns="dataset", values="n_cpts"))
-print("\n\nmean time\n")
-print(df.pivot_table(index="method", columns="dataset", values="time"))
+@click.command()
+@click.option("-n", default=1)
+def main(n):
+    last_file = sorted(files)[-int(n)]
+    df = pd.read_csv(last_file)
+
+    print("mean score\n")
+    print(df.pivot_table(index="method", columns="dataset", values="score"))
+    print("\n\nmedian score\n")
+    print(
+        df.pivot_table(
+            index="method", columns="dataset", values="score", aggfunc=np.median
+        )
+    )
+    print("\n\nmean n_cpts\n")
+    print(df.pivot_table(index="method", columns="dataset", values="n_cpts"))
+    print("\n\nmean time\n")
+    print(df.pivot_table(index="method", columns="dataset", values="time"))
+
+
+if __name__ == "__main__":
+    main()
