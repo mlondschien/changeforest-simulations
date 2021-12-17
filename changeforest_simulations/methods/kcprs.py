9480abf83da2b16b0794074fc149fb8bd05b2610
Malte Londschien <malte@londschien.de>
2021-12-17 14:50:21 +0100

    New method kcprs.

---
+++ changeforest_simulations/methods/kcprs.py
@@ -0,0 +1,23 @@
+from rpy2.robjects import default_converter, numpy2ri, r
+from rpy2.robjects.conversion import localconverter
+
+
+def kcprs(X, minimal_relative_segment_length):
+    min_size = int(minimal_relative_segment_length * X.shape[0])
+    Kmax = int(1 / minimal_relative_segment_length)
+
+    n, p = X.shape
+    with localconverter(default_converter + numpy2ri.converter):
+        r.assign("X", r.matrix(X, nrow=n, ncol=p))
+        r.assign("min_size", min_size)
+        r.assign("Kmax", Kmax)
+        return (
+            [0]
+            + list(
+                r(
+                    "kcpRS::kcpRS(data=X, RS_fun=kcpRS::runMean, RS_name='mean', wsize=2 * min_size, nperm=1000, Kmax=Kmax, alpha=0.05)$changePoints"
+                )
+                - 1
+            )
+            + [n]
+        )
--- install_packages.R
+++ install_packages.R
@@ -3,5 +3,5 @@
 # install.packages("CDFt", version="1.2", repos="http://cran.us.r-project.org")  
 # # nmcdr was removed from CRAN.
 # install.packages("https://cran.r-project.org/src/contrib/Archive/nmcdr/nmcdr_0.3.0.tar.gz")
-
+install.packages("kcpRS", version="1.0.0", repos="https://stat.ethz.ch/CRAN/")
 install.packages("KernSeg", version="0.0.2", repos="http://R-Forge.R-project.org")
