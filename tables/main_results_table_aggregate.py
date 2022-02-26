3b1675470b807a11d1dac305447bac9af38c4868
Malte Londschien <malte@londschien.de>
2022-02-26 11:29:10 +0100

    Pretty print tables in main_results_table_aggregate.py.

--- tables/main_results_table_aggregate.py
+++ tables/main_results_table_aggregate.py
@@ -9,6 +9,36 @@ import pandas as pd
 
 _OUTPUT_PATH = Path(__file__).parents[1].absolute() / "output"
 
+DATASET_RENAMING = {"change_in_mean": "CIM", "change_in_covariance": "CIC"}
+DATASET_ORDERING = [
+    "CIM",
+    "CIC",
+    "dirichlet",
+    "iris",
+    "glass",
+    "wine",
+    "breast-cancer",
+    "abalone",
+    "dry-beans",
+]
+
+METHOD_RENAMING = {
+    "change_in_mean_bs": "change in mean",
+    "changeforest_bs": "changeforest (ours)",
+    "changekNN_bs": "changekNN",
+    "ecp": "ECP",
+    "multirank": "MultiRank",
+    "kernseg_rbf": "KCP (rbf)",
+}
+METHOD_ORDERING = [
+    "change in mean",
+    "changeforest (ours)",
+    "changekNN",
+    "ECP",
+    "KCP (rbf)",
+    "MultiRank",
+]
+
 
 @click.command()
 @click.option("--file", default=None, help="Filename to use.")
@@ -26,21 +56,18 @@ def main(file, datasets, methods):
         df = df[df["dataset"].isin(datasets)]
 
     # score
-    df_print = df.groupby(["method", "dataset"])["score"].apply(
+    df_score = df.groupby(["method", "dataset"])["score"].apply(
         lambda x: f"{np.mean(x):.3f} ({np.std(x):.3f})"
     )
-    df_print = df_print.reset_index().pivot(index=["method"], columns=["dataset"])
-    df_print.columns = df_print.columns.get_level_values(level=1)
-    df_print = df_print[datasets]
-
-    print(df_print.to_latex())
+    df_score = df_score.reset_index().pivot(index=["method"], columns=["dataset"])
+    to_latex(df_score)
 
     # time
     df_print = df.groupby(["method", "dataset"])["time"].apply(
         lambda x: fmt(np.mean(x))
     )
     df_print = df_print.reset_index().pivot(index=["method"], columns=["dataset"])
-    print(df_print)
+    to_latex(df_print.to_latex())
 
 
 def fmt(x):
@@ -50,6 +77,16 @@ def fmt(x):
         return f"{x:.1f}"
     else:
         return f"{x:.2f}"
+
+
+def to_latex(df):
+    df.columns = df.get_level_values(level=1)
+    df = df.rename(columns=DATASET_RENAMING, copy=False)[DATASET_ORDERING]
+
+    df.index = df.index.rename(METHOD_RENAMING)
+    df = df.reindex(METHOD_ORDERING, axis=0)
+
+    print(df.to_latex())
 
 
 if __name__ == "__main__":
