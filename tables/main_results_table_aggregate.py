4f7de9203bcf4187de4ad12781c9d1b75220a5c0
Malte Londschien <61679398+mlondschien@users.noreply.github.com>
2022-02-26 16:14:53 +0100

    Pretty print tables in main_results_table_aggregate.py. (#59)
    * Pretty print tables in main_results_table_aggregate.py.
    
    * Simplify.
    
    * df has no attribute get_level_values.
    
    * Rename the df, not the index.
    
    * rm to_latex().
    
    * Print n_unique for verification.
    
    * pivot.

--- tables/main_results_table_aggregate.py
+++ tables/main_results_table_aggregate.py
@@ -9,38 +9,63 @@ import pandas as pd
 
 _OUTPUT_PATH = Path(__file__).parents[1].absolute() / "output"
 
+DATASET_RENAMING = {"change_in_mean": "CIM", "change_in_covariance": "CIC"}
+DATASET_ORDERING = [
+    "CIM",
+    "CIC",
+    "dirichlet",
+    "iris",
+    "glass",
+    "wine",
+    "breast-cancer",
+    "abalone",
+    "dry-beans",
+]
+
+METHOD_RENAMING = {
+    "change_in_mean_bs": "change in mean",
+    "changeforest_bs": "changeforest (ours)",
+    "changekNN_bs": "changekNN",
+    "ecp": "ECP",
+    "multirank": "MultiRank",
+    "kernseg_rbf": "KCP (rbf)",
+}
+METHOD_ORDERING = [
+    "change in mean",
+    "changeforest (ours)",
+    "changekNN",
+    "ECP",
+    "KCP (rbf)",
+    "MultiRank",
+]
+
 
 @click.command()
 @click.option("--file", default=None, help="Filename to use.")
-@click.option("--datasets", default=None, help="Datasets to benchmark. All if None.")
-@click.option("--methods", default=None, help="Methods to benchmark. All if None.")
-def main(file, datasets, methods):
+def main(file):
     df = pd.concat([pd.read_csv(f) for f in _OUTPUT_PATH.glob(f"{file}_*.csv")])
 
-    if methods is not None:
-        methods = methods.split(" ")
-        df = df[df["method"].isin(methods)]
-
-    if datasets is not None:
-        datasets = datasets.split(" ")
-        df = df[df["dataset"].isin(datasets)]
-
     # score
-    df_print = df.groupby(["method", "dataset"])["score"].apply(
+    df_score = df.groupby(["method", "dataset"])["score"].apply(
         lambda x: f"{np.mean(x):.3f} ({np.std(x):.3f})"
     )
-    df_print = df_print.reset_index().pivot(index=["method"], columns=["dataset"])
-    df_print.columns = df_print.columns.get_level_values(level=1)
-    df_print = df_print[datasets]
-
-    print(df_print.to_latex())
+    df_score = df_score.reset_index().pivot(index=["method"], columns=["dataset"])
+    to_latex(df_score)
 
     # time
-    df_print = df.groupby(["method", "dataset"])["time"].apply(
-        lambda x: fmt(np.mean(x))
+    df_time = df.groupby(["method", "dataset"])["time"].apply(lambda x: fmt(np.mean(x)))
+    df_time = df_time.reset_index().pivot(index=["method"], columns=["dataset"])
+    to_latex(df_time)
+
+    # n_unique
+    print("Comparing n_unique to n. These should be equal!")
+    df_n = (
+        df.groupby(["method", "dataset"])["seed"]
+        .apply(lambda x: f"{len(x)} ({x.nunique()})")
+        .reset_index()
+        .pivot(index=["method"], columns=["dataset"])
     )
-    df_print = df_print.reset_index().pivot(index=["method"], columns=["dataset"])
-    print(df_print)
+    print(df_n)
 
 
 def fmt(x):
@@ -50,6 +75,16 @@ def fmt(x):
         return f"{x:.1f}"
     else:
         return f"{x:.2f}"
+
+
+def to_latex(df):
+    df.columns = df.columns.get_level_values(level=1)
+    df = df.rename(columns=DATASET_RENAMING, copy=False)[DATASET_ORDERING]
+
+    df = df.rename(METHOD_RENAMING)
+    df = df.reindex(METHOD_ORDERING, axis=0)
+
+    print(df.to_latex())
 
 
 if __name__ == "__main__":
