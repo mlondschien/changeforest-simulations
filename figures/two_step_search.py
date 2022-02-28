1110266b4465c7410f3e57add5dd5d84113f73b8
Malte Londschien <malte@londschien.de>
2022-02-28 13:19:02 +0100

    New plot: two-step-search gain with biased RF.

--- figures/two_step_search.py
+++ figures/two_step_search.py
@@ -1,4 +1,5 @@
 import numpy as np
+from biosphere import RandomForest
 from changeforest import Control, changeforest
 from matplotlib import pyplot as plt
 
@@ -35,7 +36,49 @@ for idx in range(3):
     axes[idx, 1].set_ylabel("proba. prediction")
 
 axes[2, 0].set_xlabel("s")
-axes[2, 1].set_xlabel("s")
+axes[2, 1].set_xlabel("t")
 
 plt.tight_layout(pad=1.5)
 plt.savefig("figures/two_step_search.eps", dpi=300)
+
+
+# two_step_search_biased.png
+fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(11, 3))
+
+y = np.zeros(n, dtype="float")
+s = 75
+
+y[0:s] = 1
+rf = RandomForest()
+rf.fit(X, y)
+predictions = rf.predict(X)  # not _oob
+
+log_likelihoods = np.stack(
+    [
+        np.log(0.0024787521766663585 + 0.9975212478233336 * n / s * predictions),
+        np.log(
+            0.0024787521766663585 + 0.9975212478233336 * n / (n - s) * (1 - predictions)
+        ),
+    ]
+)
+
+gain = log_likelihoods[1, :].sum() + np.concatenate(
+    [[0], np.cumsum(log_likelihoods[0, :-1] - log_likelihoods[1, :-1])]
+)
+
+axes[0].plot(range(n), gain, "k")
+ymin, ymax = axes[0].get_ylim()
+axes[0].vlines(alpha[1:-1], ymin=ymin, ymax=ymax, color="green")
+axes[0].vlines(
+    np.nanargmax(gain), ymin=ymin, ymax=ymax, linestyles="dashed", color="red",
+)
+axes[0].vlines(s, ymin=ymin, ymax=ymax, linestyles="dotted", color="blue")
+axes[0].set_ylabel("approx. gain")
+axes[0].set_xlabel("s")
+
+axes[1].scatter(range(n), predictions, s=2, c="k")
+axes[1].set_ylabel("proba. prediction")
+axes[1].set_xlabel("t")
+
+plt.tight_layout()
+plt.savefig("figures/two_step_search_biased.eps", dpi=300)
