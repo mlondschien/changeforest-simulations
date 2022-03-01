b2942efc7318a1a2624142e338bbf8e33aae6b23
Malte Londschien <malte@londschien.de>
2022-03-01 11:02:28 +0100

    One fewer split.

--- changeforest_simulations/methods/ruptures.py
+++ changeforest_simulations/methods/ruptures.py
@@ -36,7 +36,7 @@ def kernseg(X, kernel, minimal_relative_segment_length, params=None, **kwargs):
     algo = rpt.KernelCPD(kernel=kernel, params=params)
 
     # ruptures requires n_bkps_max <= n / 2 - 2.
-    n_bkps_max = int(min(1 / minimal_relative_segment_length, X.shape[0] / 2 - 2))
+    n_bkps_max = int(min(1 / minimal_relative_segment_length - 1, X.shape[0] / 2 - 2))
     algo.fit(X).predict(n_bkps=n_bkps_max)
 
     segmentations_values = [[len(X)]] + list(algo.segmentations_dict.values())
