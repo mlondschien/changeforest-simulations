509e281dd8d98d1f8aef9f6d64e89130baa8782b
Malte Londschien <61679398+mlondschien@users.noreply.github.com>
2022-03-03 17:02:12 +0100

    Reorder dataset summaries. (#67)
    * Reorder dataset summaries.
    
    * Different segmentations.

--- tables/adj_rand_examples.py
+++ tables/adj_rand_examples.py
@@ -1,17 +1,33 @@
 from changeforest_simulations import adjusted_rand_score
 
-for true_changepoints, estimated_changepoints in [
-    ([0, 50, 100, 150], [0, 50, 100, 150]),
-    ([0, 50, 100, 150], [0, 52, 99, 150]),
-    ([0, 50, 100, 150], [0, 23, 50, 100, 150]),
-    ([0, 50, 100, 150], [0, 43, 87, 97, 150]),
-    ([0, 50, 100, 150], [0, 50, 150]),
-    ([0, 50, 100, 150], [0, 23, 150]),
-    ([0, 17, 46, 55, 68, 144, 214], [0, 15, 45, 55, 68, 142, 214]),
-    ([0, 17, 46, 55, 68, 144, 214], [0, 17, 46, 55, 144, 214]),
-    ([0, 17, 46, 55, 68, 144, 214], [0, 17, 46, 55, 95, 144, 214]),
-    ([0, 17, 46, 55, 68, 144, 214], [0, 50, 100, 150, 214]),
+for true_changepoints, estimated_changepoints, comment in [
+    # ([0, 50, 100, 150], [0, 50, 100, 150]),
+    # ([0, 50, 100, 150], [0, 52, 99, 150]),
+    # ([0, 50, 100, 150], [0, 23, 50, 100, 150]),
+    # ([0, 50, 100, 150], [0, 43, 87, 97, 150]),
+    # ([0, 50, 100, 150], [0, 50, 150]),
+    # ([0, 50, 100, 150], [0, 23, 150]),
+    ([0, 17, 46, 55, 68, 144, 214], [0, 17, 46, 55, 68, 144, 214], "perfect fit"),
+    (
+        [0, 17, 46, 55, 68, 144, 214],
+        [0, 15, 45, 55, 68, 142, 214],
+        "almost perfect fit",
+    ),
+    ([0, 17, 46, 55, 68, 144, 214], [0, 46, 55, 68, 144, 214], "undersegmentation"),
+    ([0, 17, 46, 55, 68, 144, 214], [0, 17, 46, 55, 144, 214], "undersegmentation"),
+    (
+        [0, 17, 46, 55, 68, 144, 214],
+        [0, 17, 46, 55, 68, 80, 144, 214],
+        "oversegmentation",
+    ),
+    (
+        [0, 17, 46, 55, 68, 144, 214],
+        [0, 17, 46, 55, 68, 100, 144, 214],
+        "oversegmentation",
+    ),
+    ([0, 17, 46, 55, 68, 144, 214], [0, 50, 100, 150, 214], "random segmentation"),
+    ([0, 17, 46, 55, 68, 144, 214], [0, 214], "no segmentation"),
 ]:
     print(
-        f"{', '.join(map(str, true_changepoints))} & {', '.join(map(str, estimated_changepoints))} & {adjusted_rand_score(true_changepoints, estimated_changepoints):.2f} \\\\"
+        f"{', '.join(map(str, true_changepoints))} & {', '.join(map(str, estimated_changepoints))} & {adjusted_rand_score(true_changepoints, estimated_changepoints):.2f} & {comment} \\\\"
     )
--- tables/dataset_summaries.py
+++ tables/dataset_summaries.py
@@ -1,8 +1,5 @@
 import numpy as np
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
-from sklearn.model_selection import train_test_split
 
 from changeforest_simulations._load import load
 from changeforest_simulations._simulate import simulate
@@ -11,6 +8,9 @@ MINIMAL_RELATIVE_SEGMENT_LENGTH = 0.01
 
 # Table summarising datasets
 for dataset in [
+    "change_in_mean",
+    "change_in_covariance",
+    "dirichlet",
     "iris",
     "glass",
     "wine",
@@ -18,9 +18,6 @@ for dataset in [
     "abalone",
     "dry-beans",
     # "covertype",
-    "change_in_mean",
-    "change_in_covariance",
-    "dirichlet",
 ]:
     if dataset in ["change_in_mean", "change_in_covariance", "dirichlet"]:
         changepoints, X = simulate(dataset)
@@ -43,24 +40,12 @@ for dataset in [
     data = data[lambda x: ~x["class"].isin(small_classes)]
 
     X, y = data.drop(columns="class").to_numpy(), data["class"].to_numpy()
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.2, random_state=0
-    )
-
-    clf = RandomForestClassifier(n_estimators=100, random_state=0)
-    clf.fit(X_train, y_train)
-    y_pred = clf.predict(X_test)
-    accuracy = accuracy_score(y_test, y_pred)
-
-    one_hot = np.apply_along_axis(lambda x: len(np.unique(x)) <= 2, 0, X).sum()
     _, value_counts = np.unique(y, return_counts=True)
 
     print(
         f"""\
 {dataset.replace('-', ' ')} & \
 {X.shape[0]} & {X.shape[1]} & \
-{one_hot} & \
 {len(np.unique(y))} & \
-${', '.join([str(x) for x in sorted(value_counts)])}$ & \
-{accuracy:.2f} \\\\"""
+${', '.join([str(x) for x in sorted(value_counts)])}$ \\\\"""
     )
