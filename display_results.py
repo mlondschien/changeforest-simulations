550246d0e47230e48d5ad41e2b79783653b3f420
Malte Londschien <malte@londschien.de>
2022-03-05 08:35:53 +0100

    Update display-results.

--- display_results.py
+++ display_results.py
@@ -4,15 +4,13 @@ import click
 import numpy as np
 import pandas as pd
 
-output_path = Path(__file__).parent.absolute() / "output"
-files = output_path.glob("202*.csv")
+_OUTPUT_PATH = Path(__file__).parent.absolute() / "output"
 
 
 @click.command()
-@click.option("-n", default=1)
-def main(n):
-    last_file = sorted(files)[-int(n)]
-    df = pd.read_csv(last_file)
+@click.option("--file")
+def main(file):
+    df = pd.concat([pd.read_csv(f) for f in _OUTPUT_PATH.glob(f"{file}_*.csv")])
 
     print(
         f"""mean score
@@ -23,6 +21,18 @@ median score
 
 mean n_cpts
 {df.pivot_table(index="method", columns="dataset", values="n_cpts")}
+
+mean left hausdorff
+{df.pivot_table(index="method", columns="dataset", values="left_hausdorff")}
+
+mean right hausdorff
+{df.pivot_table(index="method",	columns="dataset", values="right_hausdorff")}
+
+mean symmetric hausdorff
+{df.pivot_table(index="method", columns="dataset", values="symmetric_hausdorff")}
+
+median symmetric hausdorff
+{df.pivot_table(index="method",	columns="dataset", values="symmetric_hausdorff", aggfunc=np.median)}
 
 mean time
 {df.pivot_table(index="method", columns="dataset", values="time")}
