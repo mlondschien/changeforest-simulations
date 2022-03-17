61e26ce24cc8ea9da7c93003560e793ac65f95b7
Malte Londschien <malte@londschien.de>
2022-03-17 09:57:13 +0100

    Use change_in_covariance simulation setup for gain curve.

--- figures/gain.py
+++ figures/gain.py
@@ -4,22 +4,24 @@ from matplotlib import pyplot as plt
 from sklearn.neighbors import KNeighborsClassifier
 
 from changeforest_simulations import simulate
-from changeforest_simulations.constants import COLORS
+from changeforest_simulations.constants import COLORS, FIGURE_FONT_SIZE, FIGURE_WIDTH
 
-alpha, X = simulate("iris", seed=1)
+alpha, X = simulate("change_in_covariance", seed=2)
 n = X.shape[0]
 
 red = COLORS["red"]
 green = COLORS["green"]
 blue = COLORS["blue"]
-plt.rcParams.update({"font.size": 12})
+plt.rcParams.update({"font.size": FIGURE_FONT_SIZE})
 
 
 def rf_gain(s, X):
     n = X.shape[0]
     y = np.zeros(n, dtype="float")
     y[0:s] = 1
-    oob_predictions = RandomForest().fit_predict_oob(X, y)
+    oob_predictions = RandomForest(
+        max_depth=8, n_estimators=100, n_jobs=-1, max_features="sqrt"
+    ).fit_predict_oob(X, y)
 
     log_likelihoods = np.zeros(n, dtype="float")
     log_likelihoods[0:s] = np.log(
@@ -42,15 +44,18 @@ def knn_gain(s, X):
     n = X.shape[0]
     y = np.zeros(n, dtype="float")
     y[0:s] = 1
-    predictions = KNeighborsClassifier().fit(X, y).predict(X)
-
+    predictions = (
+        KNeighborsClassifier(n_jobs=1, n_neighbors=int(np.sqrt(n)))
+        .fit(X, y)
+        .predict_proba(X)[:, 1]
+    )
     log_likelihoods = np.zeros(n, dtype="float")
     log_likelihoods[0:s] = np.log(
-        0.0024787521766663585 + 0.9975212478233336 * n / (s - 1) * predictions[0:s]
+        0.0024787521766663585 + 0.9975212478233336 * n / s * predictions[0:s]
     )
     log_likelihoods[s:n] = np.log(
         0.0024787521766663585
-        + 0.9975212478233336 * n / (n - s - 1) * (1 - predictions[s:n])
+        + 0.9975212478233336 * n / (n - s) * (1 - predictions[s:n])
     )
 
     return log_likelihoods.sum()
@@ -60,28 +65,30 @@ knn_gain_curve = (
     [np.nan, np.nan] + [knn_gain(s, X) for s in range(2, n - 2)] + [np.nan, np.nan]
 )
 
-fig, axes = plt.subplots(ncols=2, figsize=(16, 3))
+fig, axes = plt.subplots(ncols=2, figsize=(FIGURE_WIDTH, FIGURE_WIDTH / 5))
 
 axes[0].plot(range(n), rf_gain_curve, "k")
 ymin, ymax = axes[0].get_ylim()
-axes[0].vlines(alpha[1:-1], ymin=ymin, ymax=ymax, linestyles="solid", color=green)
+# axes[0].vlines(alpha[1:-1], ymin=ymin, ymax=ymax, linestyles="solid", color=green)
 axes[0].vlines(
     np.nanargmax(rf_gain_curve),
     ymin=np.nanmin(rf_gain_curve),
     ymax=np.nanmax(rf_gain_curve),
-    linestyles="dashed",
+    linestyles="dotted",
     color=red,
 )
-axes[0].set_xlabel("s")
-axes[0].set_ylabel("gain(s)")
+ymin, _ = axes[0].get_ylim()
+axes[0].plot(alpha[1:-1], [ymin] * (len(alpha) - 2), "x", color=green)
+axes[0].set_xlabel("split")
+axes[0].set_ylabel("gain")
 
 axes[1].plot(range(n), knn_gain_curve, "k")
 ymin, ymax = axes[1].get_ylim()
-axes[1].vlines(alpha[1:-1], ymin=ymax, ymax=ymin, linestyles="solid", color=green)
+axes[1].plot(alpha[1:-1], [ymin] * (len(alpha) - 2), "x", color=green)
 axes[1].vlines(
-    np.nanargmax(knn_gain_curve), ymin=ymin, ymax=ymax, linestyles="dashed", color=red
+    np.nanargmax(knn_gain_curve), ymin=ymin, ymax=ymax, linestyles="dotted", color=red
 )
-axes[1].set_xlabel("s")
+axes[1].set_xlabel("split")
 # axes[1].set_ylabel("gain")
 
 plt.tight_layout()
