4f8873252faa5b12437c97ac251803ee3abd15bc
Malte Londschien <malte@londschien.de>
2022-03-17 15:30:13 +0100

    Fix tests.

--- tests/test_score.py
+++ tests/test_score.py
@@ -18,7 +18,7 @@ def test_adjusted_rand_score(left, right, expected):
 @pytest.mark.parametrize(
     "left, right, expected",
     [
-        ([0.0, 1.0, 2.0], [0, 2], 1),
+        ([0.0, 1.0, 2.0], [0, 2], 1 / 2),
         ([0, 1, 2], [0, 1, 2], 0),
         ([0, 1, 3], np.array([0, 1, 2, 3]), 0),
     ],
@@ -30,9 +30,9 @@ def test_hausdorff_distance(left, right, expected):
 @pytest.mark.parametrize(
     "left, right, expected",
     [
-        ([0.0, 1.0, 2.0], [0, 2], 1),
+        ([0.0, 1.0, 2.0], [0, 2], 1 / 2),
         ([0, 1, 2], [0, 1, 2], 0),
-        ([0, 1, 3], np.array([0, 1, 2, 3]), 1),
+        ([0, 1, 3], np.array([0, 1, 2, 3]), 1 / 3),
     ],
 )
 def test_symmetric_hausdorff_distance(left, right, expected):
