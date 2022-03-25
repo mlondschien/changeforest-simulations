3d57800560ce34051bdf44c115afb89fa4e339ed
Malte Londschien <malte@londschien.de>
2022-03-25 11:02:44 +0100

    Add mnwbs method.

--- changeforest_simulations/methods/_estimate_changepoints.py
+++ changeforest_simulations/methods/_estimate_changepoints.py
@@ -13,6 +13,7 @@ from .changeforest import (
 )
 from .ecp import ecp
 from .kcprs import kcprs
+from .mnwbs._mnwbs import mnwbs
 from .multirank.dynkw import autoDynKWRupt
 from .ruptures import kernseg_cosine, kernseg_linear, kernseg_rbf
 
@@ -80,6 +81,10 @@ def estimate_changepoints(X, method, minimal_relative_segment_length, **kwargs):
         )
     elif method == "kcprs":
         return kcprs(
+            X, minimal_relative_segment_length=minimal_relative_segment_length, **kwargs
+        )
+    elif method == "mnwbs":
+        return mnwbs(
             X, minimal_relative_segment_length=minimal_relative_segment_length, **kwargs
         )
     else:
---
+++ changeforest_simulations/methods/mnwbs/Readme.md
@@ -0,0 +1,5 @@
+Code is taken from https://github.com/hernanmp/MNWBS (6dd60cadec4b67cd7ca3fe0630320ed6048f8d41) by Oscar Hernan Madrid Padilla.
+
+See [1] for details.
+
+[1] Padilla, Oscar Hernan Madrid, et al. "Optimal nonparametric multivariate change point detection and localization." IEEE Transactions on Information Theory (2021).
