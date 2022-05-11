5dcf67d5ae24d75d2240bca7e90d059a943b9c38
Malte Londschien <malte@londschien.de>
2022-05-11 15:03:24 +0200

    Some octave magic.

--- changeforest_simulations/methods/rulsif/_rulsif.py
+++ changeforest_simulations/methods/rulsif/_rulsif.py
@@ -1,14 +1,17 @@
-# This requires octave to be installed and on the PATH
-# (see https://blink1073.github.io/oct2py/source/installation.html)
-# and oct2py (`mamba install -y oct2py`).
-from oct2py import octave
+from pathlib import Path
 
 
 def rulsif(X, minimal_relative_segment_length, **kwargs):
+    # This requires octave to be installed and on the PATH
+    # (see https://blink1073.github.io/oct2py/source/installation.html)
+    # and oct2py (`mamba install -y oct2py`).
+    from oct2py import octave
+
+    octave.addpath(str(Path(__file__).parent.absolute()))
+
     n = 50
     k = int(2 * n * minimal_relative_segment_length)
     alpha = 0.01
     n_folds = 5
-
     out = octave.change_detection(X.T, n, k, alpha, n_folds)
     return out
